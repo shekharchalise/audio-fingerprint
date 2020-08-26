e11963c65e0117416f12f626449ccdf14a2bf83f
shekharchalise <schalise@luc.id>
2020-08-25 21:02:22 -0500

    add the dynamic compressor and osciallator node

---
+++ dynamicsCompressor.js
@@ -0,0 +1,104 @@
+var audioFingerprint = (function() {
+
+    var context = null;
+    var currentTime = null;
+    var oscillator = null;
+    var compressor = null;
+    var fingerprint = null;
+    var callback = null
+
+
+    function run(cb, debug = false) {
+
+        callback = cb;
+        try {
+            setup();
+            oscillator.connect(compressor);
+            compressor.connect(context.destination);
+            oscillator.start(0);
+            context.startRendering();
+            context.oncomplete = onComplete;
+        } catch (e) {
+            if (debug) {
+                throw e;
+            }
+        }
+    }
+
+    function setup() {
+        setContext();
+        currentTime = context.currentTime;
+        setOscillator();
+        setCompressor();
+    }
+
+    function setContext() {
+        // for cross browser 
+        var audioContext = window.OfflineAudioContext || window.webkitOfflineAudioContext;
+        context = new audioContext(1, 44100, 44100); // number of channels, length, sampleRate
+    }
+
+    // The OscillatorNode interface represents a periodic waveform, such as a sine wave. It is an
+    function setOscillator() {
+        oscillator = context.createOscillator(); // create Oscillator node
+        oscillator.type = "triangle"; // A sine wave. This is the default value. 
+        // square // A square wave with a duty cycle of 0.5; that is, the signal is "high" for half of each period. // sawtooth // triangle // custom
+        oscillator.frequency.setValueAtTime(10000, currentTime); // value in hertz
+    }
+
+    /*
+    The DynamicsCompressorNode interface provides a compression effect,
+    which lowers the volume of the loudest parts of the signal in order to help prevent
+    clipping and distortion that can occur when multiple sounds are played and multiplexed
+    together at once. 
+    */
+    // https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/createDynamicsCompressor
+    function setCompressor() {
+        compressor = context.createDynamicsCompressor();
+
+        setCompressorValueIfDefined('threshold', -50);
+        setCompressorValueIfDefined('knee', 40);
+        setCompressorValueIfDefined('ratio', 12);
+        setCompressorValueIfDefined('reduction', -20);
+        setCompressorValueIfDefined('attack', 0);
+        setCompressorValueIfDefined('release', .25);
+    }
+
+    // setting the value of the compressor
+    function setCompressorValueIfDefined(item, value) {
+        if (compressor[item] !== undefined && typeof compressor[item].setValueAtTime === 'function') {
+            compressor[item].setValueAtTime(value, context.currentTime);
+        }
+    }
+
+    function onComplete(event) {
+        generateFingerprints(event);
+        compressor.disconnect();
+    }
+
+    function generateFingerprints(event) {
+        var output = null;
+        for (var i = 4500; 5e3 > i; i++) {
+            var channelData = event.renderedBuffer.getChannelData(0)[i];
+            output += Math.abs(channelData);
+        }
+
+        fingerprint = output.toString();
+
+        if (typeof callback === 'function') {
+            return callback(fingerprint);
+        }
+    }
+
+    return {
+        run: run
+    };
+
+})();
+
+
+document.querySelector("#btn").addEventListener("click", function() {
+    audioFingerprint.run(function(fingerprint) {
+        document.querySelector("#details").textContent = "AUDIO FINGERPRINT = " + fingerprint
+    });
+});
--- oscillator.js
+++ oscillator.js
@@ -1,56 +1,53 @@
-var audioFingerprintOscillator = (function () {
-	
-	var audioCtx = null;
-	var currentTime = null;
-	var oscillator = null;
-	var cc_output = [];
+var audioFingerprintOscillator = (function() {
+
+    var audioCtx = null;
+    var currentTime = null;
+    var oscillator = null;
+    var cc_output = [];
     var callback = null;
     var fingerprint = null;
 
-	
-	function run(cb, debug = false) {
-		
-		callback = cb;
-		
-		try {
-			setup();
-			oscillator.start(0);
-			generateFingerprints()
-		} catch (e) {
-			if (debug) {
-				throw e;
-			}
-		}
-	}
-	
-	function setup()
-	{
-		setContext();
-		currentTime = audioCtx.currentTime;
-		setOscillator();
-		
-	}
 
-	function setContext()
-	{
-		// for cross browser 
+    function run(cb, debug = false) {
+
+        callback = cb;
+
+        try {
+            setup();
+            oscillator.start(0);
+        } catch (e) {
+            if (debug) {
+                throw e;
+            }
+        }
+    }
+
+    function setup() {
+        setContext();
+        currentTime = audioCtx.currentTime;
+        setOscillator();
+
+    }
+
+    function setContext() {
+        // for cross browser 
         audioCtx = new(window.AudioContext || window.webkitAudioContext);
-	}
+    }
 
-	 // The OscillatorNode interface represents a periodic waveform, such as a sine wave. It is an
-	function setOscillator()
-	{
+    // The OscillatorNode interface represents a periodic waveform, such as a sine wave. It is an
+    function setOscillator() {
+        console.log("test")
         oscillator = audioCtx.createOscillator(),
-        analyser = audioCtx.createAnalyser(),
-        gain = audioCtx.createGain(),
-        scriptProcessor = audioCtx.createScriptProcessor(4096, 1, 1);
+            analyser = audioCtx.createAnalyser(),
+            gain = audioCtx.createGain(),
+            scriptProcessor = audioCtx.createScriptProcessor(4096, 1, 1);
         gain.gain.value = 0; // Disable volume
         oscillator.type = "triangle"; // Set oscillator to output triangle wave
         oscillator.connect(analyser); // Connect oscillator output to analyser input
         analyser.connect(scriptProcessor); // Connect analyser output to scriptProcessor input
         scriptProcessor.connect(gain); // Connect scriptProcessor output to gain input
         gain.connect(audioCtx.destination); // Connect gain output to audiocontext destination
-        scriptProcessor.onaudioprocess = function (bins) {
+        scriptProcessor.onaudioprocess = function(bins) {
             bins = new Float32Array(analyser.frequencyBinCount);
             analyser.getFloatFrequencyData(bins);
             for (var i = 0; i < bins.length; i = i + 1) {
@@ -61,25 +58,26 @@ var audioFingerprintOscillator = (function () {
             scriptProcessor.disconnect();
             gain.disconnect();
         };
-	}
-	
-	function generateFingerprints() {
+        generateFingerprints()
+    }
 
-        fingerprint = cc_output.slice(0,30).toString();
-		if (typeof callback === 'function') {
-			return callback(fingerprint);
-		}
-	}
-	
-	return {
-		run:run
-	};
-	
+    function generateFingerprints() {
+        fingerprint = cc_output.slice(0, 30).toString();
+        if (typeof callback === 'function') {
+            return callback(fingerprint);
+        }
+    }
+
+    return {
+        run: run
+    };
+
 })();
 
 
-document.querySelector("#btn-osci").addEventListener("click", function () {
-	audioFingerprintOscillator.run(function (fingerprint) {
-		document.querySelector("#oscillator").textContent = "AUDIO FINGERPRINT OSCILLATOR = " + fingerprint
-	});
+document.querySelector("#btn-osci").addEventListener("click", function() {
+    audioFingerprintOscillator.run(function(fingerprint) {
+        document.querySelector("#oscillator").textContent = "AUDIO FINGERPRINT OSCILLATOR = " + fingerprint
+    });
+
 });
